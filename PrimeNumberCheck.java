2d76f27ea6c376a9b2f57ca3fa98628ae96b7f28
unknown <yashwanthreddy670@.com>
2024-11-26 19:27:01 +0530

    second commit

--- PrimeNumberCheck.java
+++ PrimeNumberCheck.java
@@ -1,6 +1,6 @@
 public class PrimeNumberCheck {
     public static void main(String[] args) {
-        int num = 29; // Change this value to check different numbers
+        int num = 29; 
 
         if (isPrime(num)) {
             System.out.println(num + " is a prime number.");
@@ -11,13 +11,13 @@ public class PrimeNumberCheck {
 
     public static boolean isPrime(int num) {
         if (num <= 1) {
-            return false; // Numbers <= 1 are not prime
+            return false; 
         }
         for (int i = 2; i <= Math.sqrt(num); i++) {
             if (num % i == 0) {
-                return false; // Divisible by a number other than 1 and itself
+                return false; 
             }
         }
-        return true; // Prime if no divisors found
+        return true; 
     }
 }
